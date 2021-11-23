17c0420bf8c7cd42480131f0f46ff58cd4711efa
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-11-23 08:07:56 +0200

    v2.0.4

--- index.ts
+++ index.ts
@@ -117,7 +117,7 @@ const cssChunks: PluginImpl<InputPluginOptions> = function (options = {}) {
                         if (bundle[c]) {
                             code += (<OutputChunk>bundle[c]).imports.filter(filter)
                                 .map(f => `@import '${
-									path.relative(path.dirname(chunk.fileName), f).replace('\\', '/')
+									path.relative(path.dirname(chunk.fileName), f).replace(/\\/g, '/')
 								}';`).join('');
                         }
                     }
